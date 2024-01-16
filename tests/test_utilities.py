ee27e667089bba4bb901775ab28066acbaf0aedb
Ian Hopkinson <ianhopkinson@googlemail.com>
2024-01-16 09:57:22 +0000

    Reorganise testing a bit

--- tests/test_utilities.py
+++ tests/test_utilities.py
@@ -1,7 +1,17 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
-from hdx_scraper_climada.utilities import read_attributes
+import csv
+import os
+from pathlib import Path
+from hdx_scraper_climada.utilities import read_attributes, write_dictionary
+
+TEMP_FILE_PATH = os.path.join(Path(__file__).parent, "temp", "tmp.csv")
+DICT_LIST = [
+    {"a": 1, "b": 2, "c": 3},
+    {"a": 4, "b": 5, "c": 6},
+    {"a": 7, "b": 8, "c": 9},
+]
 
 
 def test_read_attributes():
@@ -11,3 +21,17 @@ def test_read_attributes():
     assert len(dataset_attributes["resource"]) == 2
     assert dataset_attributes["skip_country"] == ["SYR"]
     assert dataset_attributes["data_type_group"] == "hazard"
+
+
+def test_write_dictionary_to_local_file():
+    if os.path.isfile(TEMP_FILE_PATH):
+        os.remove(TEMP_FILE_PATH)
+
+    status = write_dictionary(TEMP_FILE_PATH, DICT_LIST)
+    with open(TEMP_FILE_PATH, encoding="utf-8") as temp_file:
+        rows_read = list(csv.DictReader(temp_file))
+
+    assert len(rows_read) == 3
+    assert rows_read[0] == {"a": "1", "b": "2", "c": "3"}
+    assert "New file" in status
+    assert "is being created" in status
